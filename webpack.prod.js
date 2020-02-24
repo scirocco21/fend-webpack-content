f2400377385f8362ac9fe005b2f2d6bad59f42c6
scirocco21 <sebgertz@gmail.com>
2020-02-23 20:40:39 -0500

    add sass loader

--- webpack.prod.js
+++ webpack.prod.js
@@ -6,11 +6,17 @@ module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
     module: {
+      
         rules: [
+          
             {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+              test: /\.scss$/,
+              use: ['style-loader', 'css-loader', 'sass-loader']
             }
         ]
     },
